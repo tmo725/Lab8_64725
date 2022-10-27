20cff8d618abd824810f75ebafddc00f2dafc6c3
tmo725 <113779402+tmo725@users.noreply.github.com>
2022-10-27 23:39:17 +0700

    Add files via upload

---
+++ q11.sh
@@ -0,0 +1,26 @@
+for file in $(find . -type f -name "*.c")
+do
+   prog_name=`echo $file | cut -d'/' -f2 | cut -d'.' -f1`
+   prog=$(echo $file | cut -d'/' -f2)
+   gcc $prog -o $prog_name 2> /dev/null
+   if [ -f $prog_name ]
+   then
+      ans=$(./$prog_name)
+      if [ "$ans" = 20 ]
+      then 
+      score=10
+      elif [ "$ans" != 20 ]
+      then
+      score=7
+      fi
+   else
+      score=5
+   fi
+   printf "%s.c %s\n" $prog_name $score
+done
+   
+
+
+
+
+
